df7899ec7207fba41e2af44a8e5642dbb3548ab9
noveminfo <noveminfo@gmail.com>
2024-04-15 14:36:07 +0900

    react native, tailwindcss, typescript, expo-router, appwrite

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,113 @@
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
+
+const SignUp = () => {
+  const [form, setForm] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    if (!form.userName || !form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all the fields");
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const result = await createUser(form.userName, form.email, form.password);
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full flex justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+
+          <Text className="text-2xl text-white font-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+
+          <FormField
+            title="Username"
+            value={form.userName}
+            placeholder="Username"
+            handleChangeText={(e) =>
+              setForm({
+                ...form,
+                userName: e,
+              })
+            }
+            otherStyles="mt-7"
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            placeholder="Email"
+            handleChangeText={(e) =>
+              setForm({
+                ...form,
+                email: e,
+              })
+            }
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            placeholder="Password"
+            handleChangeText={(e) =>
+              setForm({
+                ...form,
+                password: e,
+              })
+            }
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
---
+++ lib/appwrite.ts
@@ -0,0 +1,71 @@
+import { Client, Account, ID, Avatars, Databases } from "react-native-appwrite";
+
+export const config = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.noveminfo.aora",
+  projectId: "661be350c3979b6063b3",
+  databaseId: "661be466d027b86d967b",
+  userCollectionId: "661be4a49fa4020bcbfa",
+  videoCollectionId: "661be51f488bc2a4e394",
+  storageId: "661be706e0cdcc99bf09",
+};
+
+const client = new Client();
+client
+  .setEndpoint(config.endpoint)
+  .setProject(config.projectId)
+  .setPlatform(config.platform);
+
+const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+export const createUser = async (
+  username: string,
+  email: string,
+  password: string
+) => {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(username);
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+      config.databaseId,
+      config.userCollectionId,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email,
+        username,
+        avatar: avatarUrl,
+      }
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+export async function signIn(email: string, password: string) {
+  try {
+    const session = await account.createEmailSession(email, password);
+
+    return session;
+  } catch (error) {
+    console.log(error);
+    // return error;
+    throw new Error(error);
+  }
+}
